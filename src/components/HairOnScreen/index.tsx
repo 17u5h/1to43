242ef585749a4ba2409b12911464356a3d4b2315
17u5h <102058870+17u5h@users.noreply.github.com>
2023-06-01 23:33:31 +0700

    removing of hair prank added

--- src/components/HairOnScreen/index.tsx
+++ src/components/HairOnScreen/index.tsx
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 import hairOnScreen from '../../assets/pictures/hair-on-screen.png'
 import * as S from "./styles";
 
 
 const HairOnScreen = () => {
+	const [isHairVisible, setHarVisible] = useState<boolean>(true)
+	const removeHair = () => {
+		setHarVisible(false)
+	}
 
 	return (
-		<S.HairOnScreen src={hairOnScreen}>
-
-		</S.HairOnScreen>
+		<>
+		{isHairVisible && <S.HairOnScreen src={hairOnScreen} onMouseDown={removeHair}/>}
+		</>
 	);
 };
 
